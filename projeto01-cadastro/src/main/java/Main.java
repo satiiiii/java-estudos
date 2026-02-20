22d0615315c69b09c5a1acc7a94c0f92dc345087
satiiiii <gabcunha733@gmail.com>
2026-02-20 07:03:50 -0300

    feat: encapsulamento com getters e setters

--- projeto01-cadastro/src/main/java/Main.java
+++ projeto01-cadastro/src/main/java/Main.java
@@ -1,24 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-        Pessoa pessoa1 = new Pessoa();
-        pessoa1.nome = "Gabriel";
-        pessoa1.idade = 21;
-        pessoa1.salario = 2300.50;
-
+        Pessoa pessoa1 = new Pessoa("Gabriel", 21, 2350.50);
         pessoa1.seApresentar();
 
-        Pessoa pessoa2 = new Pessoa();
-        pessoa2.nome = "Stefani";
-        pessoa2.idade = 32;
-        pessoa2.salario = 5000.00;
-
+        Pessoa pessoa2 = new Pessoa( "Stefani", 32, 5000.00);
         pessoa2.seApresentar();
 
-        Pessoa pessoa3 = new Pessoa();
-        pessoa3.nome = "Roberto";
-        pessoa3.idade = 42;
-        pessoa3.salario = 15500.00;
-
+        Pessoa pessoa3 = new Pessoa("Roberto", 45, 14500.00);
         pessoa3.seApresentar();
     }
 }
--- projeto01-cadastro/src/main/java/Pessoa.java
+++ projeto01-cadastro/src/main/java/Pessoa.java
@@ -1,7 +1,38 @@
 public class Pessoa {
-    String nome;
-    int idade;
-    double salario;
+    public Pessoa(String nome, int idade, double salario) {
+        this.nome = nome;
+        this.idade = idade;
+        this.salario = salario;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public void setSalario(double salario) {
+        this.salario = salario;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public double getSalario() {
+        return salario;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    private String nome;
+    private int idade;
+    private double salario;
+
     public void seApresentar() {
         System.out.println("Ola! Meu nome é " + nome + " tenho " + idade + " anos!");
     }
